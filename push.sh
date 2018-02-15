cf1c91eaef731a069d0202ef5557594b5fc35520
Eric Haas <ehaas@healthedatainc.com>
2018-02-15 00:10:14 -0800

    update docs with latest

--- push.sh
+++ push.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 name="Argo-Scheduling"
-path1 = "../IG-Template2"
+while getopts s option
+do
+ case "${option}"
+ in
+ s) path1='../IG-Template2';;
+ esac
+done
 echo "================================================================="
 echo === Push $name IG!!! $(date -u) to github ===
 echo see '/Users/ehaas/Documents/FHIR/IG-Template2/my_notes/local_workflow.mdlocal' file for how to use:
-echo copy the locals docs directory from the $path1 directory
-echo to this directory before pushing so have a public version
-echo in GitHub Pages
 echo "================================================================="
-echo cp -r $path1/docs
+if [[ $path1 ]]; then
+  echo "================================================================="
+  echo copy the locals docs directory from the $path1 directory
+  echo to this directory before pushing so have a public version
+  echo in GitHub Pages
+  echo "================================================================="
+  rm -r docs/*
+  cp -r $path1/docs .
+fi
 git status
 git add .
 git status
